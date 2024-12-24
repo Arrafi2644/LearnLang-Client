20d2ef7d9663c200a873590897d6bf2694a73cce
Arrafi2644 <raffian2644@gmail.com>
2024-12-24 10:09:54 +0600

    Complete add tutorial funtionality

--- src/pages/AddTutor/AddTutor.jsx
+++ src/pages/AddTutor/AddTutor.jsx
@@ -1,71 +1,104 @@
 import React from 'react';
+import useAuth from '../../hooks/UseAuth';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const AddTutor = () => {
-    return (
-            // <h2 className='te xt-3xl font-bold text-center'>Add a Tutor</h2>
-           
-            <div className="hero bg-background-light my-10 md:my-14 rounded-lg min-h-screen">
-                
-            <div className="hero-content flex-col w-full md:w-11/12 lg:w-4/5">
-              <div className="text-center lg:text-left w-full ">
-               <h1 className="text-3xl font-bold text-center">Add Tutorial</h1>
-                 <p className="py-6 text-center">
-                 Add tutorials to connect with learners and share your teaching expertise globally.
-                 </p>
-              </div>
-              <div className="card bg-base-100 w-full rounded-lg shadow-2xl">
-                <form className="card-body grid grid-cols-2 gap-2">
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text">Name</span>
-                    </label>
-                    <input type="text" placeholder="Name" className="input input-bordered" required />
-                  </div>
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text">Email</span>
-                    </label>
-                    <input type="email" placeholder="Email" className="input input-bordered" required />
-                  </div>
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text">Tutorial Image</span>
-                    </label>
-                    <input type="url" placeholder="Tutorial image url" className="input input-bordered" required />
-                  </div>
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text">Language</span>
-                    </label>
-                    <input type="text" placeholder="Language" className="input input-bordered" required />
-                  </div>
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text">Price</span>
-                    </label>
-                    <input type="text" placeholder="Price" className="input input-bordered" required />
-                  </div>
-                   <div className="form-control">
-                    <label className="label">
-                      <span className="label-text">Review</span>
-                    </label>
-                    <input type="text" value={0} readOnly placeholder="Review" className="input input-bordered" required />
-                  </div>
-                  <div className="form-control col-span-1 md:col-span-2">
-                    <label className="label">
-                      <span className="label-text">Description</span>
-                    </label>
-                    <textarea className="textarea textarea-bordered resize-none" placeholder="Description" required></textarea>
-                  </div>
-                  
-                  <div className="form-control mt-6 col-span-1 md:col-span-2">
-                    <button className="btn btn-primary">Add Tutorial</button>
-                  </div>
-                </form>
-              </div>
+
+  const {user} = useAuth()
+  // console.log(user)
+
+  const handleAddTutorial = (e) => {
+    e.preventDefault()
+
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const image = form.tutorialImage.value;
+    const language = form.language.value;
+    const price = form.price.value;
+    const review = form.review.value;
+    const description = form.description.value;
+
+    const tutorialInfo = {name, email, image, language, price, review, description}
+    console.log(tutorialInfo);
+
+    axios.post(`http://localhost:5000/tutors`, tutorialInfo)
+    .then(res => {
+      console.log(res.data);
+      if(res.data.insertedId){
+        toast.success("Tutorial added successful!")
+      }
+    })
+    .catch(err => {
+      toast.success("Something went wrong! Please try again.")
+    })
+    
+  }
+  return (
+    // <h2 className='te xt-3xl font-bold text-center'>Add a Tutor</h2>
+
+    <div className="hero bg-background-light my-10 md:my-14 rounded-lg min-h-screen">
+      <div className="hero-content flex-col w-full md:w-11/12 lg:w-4/5">
+        <div className="text-center lg:text-left w-full ">
+          <h1 className="text-3xl font-bold text-center">Add Tutorial</h1>
+          <p className="py-6 text-center">
+            Add tutorials to connect with learners and share your teaching expertise globally.
+          </p>
+        </div>
+        <div className="card bg-base-100 w-full rounded-lg shadow-2xl">
+          <form onSubmit={handleAddTutorial} className="card-body grid grid-cols-2 gap-2">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input name='name' type="text" placeholder="Name" value={user.displayName} readOnly className="input input-bordered" required />
             </div>
-          </div>
-    );
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input name='email' type="email" placeholder="Email" value={user.email} readOnly className="input input-bordered" required />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Tutorial Image</span>
+              </label>
+              <input name='tutorialImage' type="url" placeholder="Tutorial image url" className="input input-bordered" required />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Language</span>
+              </label>
+              <input name='language' type="text" placeholder="Language" className="input input-bordered" required />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Price</span>
+              </label>
+              <input name='price' type="text" placeholder="Price" className="input input-bordered" required />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Review</span>
+              </label>
+              <input name='review' type="text" value={0} readOnly placeholder="Review" className="input input-bordered" required />
+            </div>
+            <div className="form-control col-span-1 md:col-span-2">
+              <label className="label">
+                <span className="label-text">Description</span>
+              </label>
+              <textarea name='description' className="textarea textarea-bordered resize-none" placeholder="Description" required></textarea>
+            </div>
+
+            <div className="form-control mt-6 col-span-1 md:col-span-2">
+              <button className="btn bg-accent-light">Add Tutorial</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddTutor;
