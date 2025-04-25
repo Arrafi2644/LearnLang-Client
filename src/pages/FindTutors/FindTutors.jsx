d7d2e813a220df05bf3352b6d4024262d91d5e16
Arrafi2644 <raffian2644@gmail.com>
2025-04-25 10:26:33 +0600

    Added: Loading in find tutorial page

--- src/pages/FindTutors/FindTutors.jsx
+++ src/pages/FindTutors/FindTutors.jsx
@@ -4,130 +4,109 @@ import TutorCard from '../../components/TutorCard/TutorCard';
 import axios from 'axios';
 import useAuth from '../../hooks/UseAuth';
 import { IoSearchOutline } from 'react-icons/io5';
+import useTutors from '../../hooks/useTutors';
 
 const FindTutors = () => {
     const { setAllTutors } = useAuth()
-    const [tutors, setTutors] = useState([])
     const [search, setSearch] = useState("")
     const params = useParams()
-    // console.log(params);
-    // console.log(params)
-    const category = params.category
-
-    // console.log(params);
+   
+    const [tutors, isLoading, refetch] = useTutors()
 
     // useEffect(() => {
-    //     axios.get(`https://learn-lang-server-rose.vercel.app/tutors/${search}`)
+
+    //     let url = `https://learn-lang-server-rose.vercel.app/tutors`
+    //     if (category) {
+    //         url = `https://learn-lang-server-rose.vercel.app/tutors?language=${category}`
+    //     }
+
+    //     axios.get(url)
     //         .then(res => {
-    //             setTutors(res.data);
-    //             setAllTutors(res.data);
-    //         })
-    // }, [search])
+    //             setTutors(res.data)
+    //             setAllTutors(res.data)
+    //         }
+    //         )
+
+    // }, [setTutors, category])
 
 
-    // useEffect(() => {
-    //     axios.get(`https://learn-lang-server-rose.vercel.app/tutors/language/${category}`)
+    // const handleSearch = (e) => {
+    //     // console.log(e.target.value);
+    //     const keyword = e.target.value;
+
+    //     axios.get(`https://learn-lang-server-rose.vercel.app/tutors/search/language/${keyword}`)
     //         .then(res => {
-    //             setTutors(res.data);
-    //             setAllTutors(res.data);
+    //             // console.log(res.data);
+    //             setTutors(res.data)
+
     //         })
-    // }, [category])
-
-    // useEffect(() => {
-    //     axios.get(`https://learn-lang-server-rose.vercel.app/tutors`)
-    //         .then(res => {
-    //             setTutors(res.data);
-    //             setAllTutors(res.data);
+    //         .catch(err => {
+    //             console.log(err);
     //         })
-    // }, [])
-
-    useEffect(() => {
-
-        let url = `https://learn-lang-server-rose.vercel.app/tutors`
-        if (category) {
-            url = `https://learn-lang-server-rose.vercel.app/tutors?language=${category}`
-        }
-
-        axios.get(url)
-            .then(res => {
-                setTutors(res.data)
-                setAllTutors(res.data)
-            }
-            )
-
-    }, [setTutors, category])
+    // }
 
 
-    const handleSearch = (e) => {
-        // console.log(e.target.value);
-        const keyword = e.target.value;
+    // const handleSort = () => {
+    //     const allTutors = tutors;
+    //     // console.log(allEquipments);
+    //     // console.log("clicked on sort");
+    //     const sortAllTutors = [...allTutors].sort(function (a, b) {
+    //         return a.price - b.price
+    //     })
 
-        axios.get(`https://learn-lang-server-rose.vercel.app/tutors/search/language/${keyword}`)
-            .then(res => {
-                // console.log(res.data);
-                setTutors(res.data)
+    //     // console.log(sortAllEquipments);
 
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }
+    //     setTutors(sortAllTutors);
 
-
-    const handleSort = () => {
-        const allTutors = tutors;
-        // console.log(allEquipments);
-        // console.log("clicked on sort");
-        const sortAllTutors = [...allTutors].sort(function (a, b) {
-            return a.price - b.price
-        })
-
-        // console.log(sortAllEquipments);
-
-        setTutors(sortAllTutors);
-
-    }
+    // }
 
 
     return (
         <div className='my-10 md:my-14 px-4 container mx-auto min-h-screen'>
-            <div className='flex flex-col md:flex-row gap-6 justify-center md:justify-between'>
-                <div><h2 className='text-3xl md:text-4xl text-primary-light font-bold text-center md:text-left'>Find Your Tutor</h2></div>
-                <div className='flex items-center gap-4'>
-                    {/* search input  */}
-                    <label className="input relative flex  items-center bg-white border border-gray-400 min-h-0 h-auto p-2 px-4 pl-2 w-full">
+         {
+            isLoading?
+            <span className="loading loading-ring loading-lg text-primary-light  absolute left-1/2 -translate-x-1/2 top-24"></span>
+            :
+   <div>
+             <div className='flex flex-col md:flex-row gap-6 justify-center md:justify-between'>
+            <div><h2 className='text-3xl md:text-4xl text-primary-light font-bold text-center md:text-left'>Find Your Tutor</h2></div>
+            <div className='flex items-center gap-4'>
+                {/* search input  */}
+                <label className="input relative flex  items-center bg-white border border-gray-400 min-h-0 h-auto p-2 px-4 pl-2 w-full">
 
-                        <span className='pr-1'><IoSearchOutline /> </span>
-                        <input type="search" className="grow" placeholder="Search" />
+                    <span className='pr-1'><IoSearchOutline /> </span>
+                    <input type="search" className="grow" placeholder="Search" />
 
-                    </label>
+                </label>
 
-                    {/* sort  */}
-                    <fieldset className="fieldset min-h-0 h-auto border border-gray-400 rounded-md">
-                        <select defaultValue="Pick a browser" className="select min-h-0 h-auto py-1.5 px-4 min-w-40 bg-white">
-                            <option>Sort By</option>
-                            <option>Ascending</option>
-                            <option>Descending</option>
-                        </select>
-                    </fieldset>
-                </div>
-
-
+                {/* sort  */}
+                <fieldset className="fieldset min-h-0 h-auto border border-gray-400 rounded-md">
+                    <select defaultValue="Pick a browser" className="select text-sm min-h-0 h-auto py-2 px-4 min-w-40 bg-white">
+                        <option>Sort By</option>
+                        <option>Ascending</option>
+                        <option>Descending</option>
+                    </select>
+                </fieldset>
             </div>
-            {
-                tutors.length > 0 ? <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6'>
-                    {
-                        tutors.map(tutor => <TutorCard key={tutor._id}
-                            tutor={tutor}
-                        // handleTutorDetails={handleTutorDetails}
-                        ></TutorCard>)
 
-                    }
+
+        </div>
+        {
+            tutors.length > 0 ? <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6'>
+                {
+                    tutors.map(tutor => <TutorCard key={tutor._id}
+                        tutor={tutor}
+                    // handleTutorDetails={handleTutorDetails}
+                    ></TutorCard>)
+
+                }
+            </div>
+                : <div className='w-f'>
+                    <h2 className='text-xl font-bold text-center'>No tutor found for this language.</h2>
                 </div>
-                    : <div className='w-f'>
-                        <h2 className='text-xl font-bold text-center'>No tutor found for this language.</h2>
-                    </div>
-            }
+        }
+   </div>
+         }
 
         </div>
     );
