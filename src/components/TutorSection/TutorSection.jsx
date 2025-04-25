390053b5e03d68c4daef1fbee24b6fe74e47252a
Arrafi2644 <raffian2644@gmail.com>
2025-04-25 09:19:29 +0600

    Fixed: tutor section slider navigation issue

--- src/components/TutorSection/TutorSection.jsx
+++ src/components/TutorSection/TutorSection.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import useTutors from '../../hooks/useTutors';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -5,8 +6,8 @@ import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
-import './swiper.css'; // optional for custom styling
-
+import './swiper.css';
+import { Link } from 'react-router-dom';
 
 const TutorSection = () => {
     const [tutors, isLoading, refetch] = useTutors();
@@ -17,43 +18,41 @@ const TutorSection = () => {
                 Our Expert Tutors
             </h2>
 
-            <Swiper
-                slidesPerView={1}
-                spaceBetween={30}
-                autoplay={{
-                    delay: 2000
-                }}
-                loop={true}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                modules={[Pagination, Navigation, Autoplay]}
-                className="mySwiper"
-                breakpoints={{
-                    320: { slidesPerView: 1 },
-                    640: { slidesPerView: 2 },
-                    768: { slidesPerView: 3 },
-                    1024: { slidesPerView: 4 },
-                }}
-            >
-
-                {tutors.map((tutor, index) => (
-                    <SwiperSlide key={index}>
-                        <div className='flex flex-col gap-1 justify-center items-center p-6 bg-white rounded-lg'>
-                            <img
-                                className='w-full h-60 border object-cover rounded-md'
-                                src={tutor.image}
-                                alt={tutor.name}
-                            />
-                            <h3 className='font-semibold text-center mt-2'>{tutor.name}</h3>
-                            <h3 className='font-semibold text-center mt-2'>{tutor.language}</h3>
-                            <button className="btn bg-[#EB5A3C] text-white min-h-0 h-auto w-full py-2.5 mt-1">View details</button>
-                            
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+            <div className='max-h-96 h-96 '>
+                {!isLoading && tutors?.length > 0 && (
+                    <Swiper
+                        slidesPerView={3}
+                        spaceBetween={30}
+                        autoplay={{ delay: 2000 }}
+                        loop={true}
+                        pagination={{ clickable: true }}
+                        navigation={true}
+                        modules={[Pagination, Navigation, Autoplay]}
+                        className="mySwiper"
+                        breakpoints={{
+                            320: { slidesPerView: 1 },
+                            640: { slidesPerView: 2 },
+                            768: { slidesPerView: 3 },
+                            1024: { slidesPerView: 4 },
+                        }}
+                    >
+                        {tutors.map((tutor, index) => (
+                            <SwiperSlide key={index}>
+                                <div className='flex flex-col gap-1 justify-center items-center p-6 bg-white rounded-lg'>
+                                    <img
+                                        className='w-full h-60 border object-cover rounded-md'
+                                        src={tutor.image}
+                                        alt={tutor.name}
+                                    />
+                                    <h3 className='font-semibold text-center mt-2'>{tutor.name}</h3>
+                                    <h3 className='font-semibold text-center mt-2'>{tutor.language}</h3>
+                                    <button className="btn bg-[#EB5A3C] text-white min-h-0 h-auto w-full py-2.5 mt-1">View details</button>
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                )}
+            </div>
         </div>
     );
 };
